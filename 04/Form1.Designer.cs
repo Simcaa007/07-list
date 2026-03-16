2049135de5145c76aeac930a3f0bf1cb686a8bdd
Simcaa007 <havelkova.si@seznam.cz>
2026-03-16 20:34:56 +0100

    hotovo

---
+++ 04/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace _04
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(133, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "kok na dik muz lyp";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(39, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "uloz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(21, 79);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(34, 22);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "a";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(61, 79);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(34, 22);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "e";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(101, 79);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(34, 22);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "i";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Location = new System.Drawing.Point(21, 107);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(34, 22);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "o";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Enabled = false;
+            this.button6.Location = new System.Drawing.Point(61, 107);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(34, 22);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "u";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Enabled = false;
+            this.button7.Location = new System.Drawing.Point(101, 107);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(34, 22);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "y";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(161, 141);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ 04/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _04
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<char> znaky = new List<char>();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string retezec = textBox1.Text;
+            foreach (char ch in retezec)
+            {
+                znaky.Add(ch);
+            }
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = true;
+            button6.Enabled = true;
+            button7.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string vys = "";
+            textBox1.Clear();
+
+            znaky.RemoveAll(a => a == 'a');
+
+            foreach (char ch in znaky)
+            {
+                textBox1.Text += ch;
+            }
+
+            button2.Enabled = false;
+
+        }
+
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            string vys = "";
+            textBox1.Clear();
+
+            znaky.RemoveAll(a => a == 'e');
+
+            foreach (char ch in znaky)
+            {
+                textBox1.Text += ch;
+            }
+
+            button3.Enabled = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string vys = "";
+            textBox1.Clear();
+
+            znaky.RemoveAll(a => a == 'i');
+
+            foreach (char ch in znaky)
+            {
+                textBox1.Text += ch;
+            }
+
+            button4.Enabled = false;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string vys = "";
+            textBox1.Clear();
+
+            znaky.RemoveAll(a => a == 'o');
+
+            foreach (char ch in znaky)
+            {
+                textBox1.Text += ch;
+            }
+
+            button5.Enabled = false;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string vys = "";
+            textBox1.Clear();
+
+            znaky.RemoveAll(a => a == 'u');
+
+            foreach (char ch in znaky)
+            {
+                textBox1.Text += ch;
+            }
+
+            button6.Enabled = false;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string vys = "";
+            textBox1.Clear();
+
+            znaky.RemoveAll(a => a == 'y');
+
+            foreach (char ch in znaky)
+            {
+                textBox1.Text += ch;
+            }
+
+            button7.Enabled = false;
+        }
+    }
+}
